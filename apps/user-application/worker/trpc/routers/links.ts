9250acbd010d1e889f6ccd4afce6684114b46bc3
sunwrobert <sunwrobert@gmail.com>
2025-12-30 22:58:52 +1100

    Add new TRPC procedures for link analytics
    - Introduced new query procedures in `links.ts` for active links, total link clicks in the last hour, last 24 hours clicks with percent change, last 30 days clicks, and clicks by country.
    - Integrated dummy data for analytics to enhance link management insights.

--- apps/user-application/worker/trpc/routers/links.ts
+++ apps/user-application/worker/trpc/routers/links.ts
@@ -13,7 +13,29 @@ import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { t } from "@/worker/trpc/trpc-instance";
 
+import { ACTIVE_LINKS_LAST_HOUR, LAST_30_DAYS_BY_COUNTRY } from "./dummy-data";
+
 export const linksTrpcRoutes = t.router({
+  activeLinks: t.procedure.query(async () => {
+    return ACTIVE_LINKS_LAST_HOUR;
+  }),
+  totalLinkClickLastHour: t.procedure.query(async () => {
+    return 13;
+  }),
+  last24HourClicks: t.procedure.query(async () => {
+    return {
+      last24Hours: 56,
+      previous24Hours: 532,
+      percentChange: 12,
+    };
+  }),
+  last30DaysClicks: t.procedure.query(async () => {
+    return 78;
+  }),
+  clicksByCountry: t.procedure.query(async () => {
+    return LAST_30_DAYS_BY_COUNTRY;
+  }),
+
   linkList: t.procedure
     .input(
       z.object({
