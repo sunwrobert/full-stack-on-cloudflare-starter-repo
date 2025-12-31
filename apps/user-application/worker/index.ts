05e01f3ea273e4f0690bfeec0934f4769d0042ad
sunwrobert <sunwrobert@gmail.com>
2025-12-31 22:43:23 +1100

    Update worker configuration and refactor request handling
    - Added `VITE_BASE_HOST` and `BACKEND_SERVICE` bindings to `worker-configuration.d.ts` and `wrangler.jsonc`.
    - Introduced a new Hono app in `hono/app.ts` to streamline request handling, replacing the previous TRPC setup.
    - Updated the main fetch handler in `index.ts` to utilize the new Hono app for processing requests, enhancing modularity and maintainability.

--- apps/user-application/worker/index.ts
+++ apps/user-application/worker/index.ts
@@ -1,22 +1,9 @@
 import { initDatabase } from "@repo/data-ops/database";
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { createContext } from "./trpc/context";
-import { appRouter } from "./trpc/router";
+import { App } from "./hono/app";
 
 export default {
   fetch(request, env, ctx) {
-    const url = new URL(request.url);
     initDatabase(env.DB);
-
-    if (url.pathname.startsWith("/trpc")) {
-      return fetchRequestHandler({
-        endpoint: "/trpc",
-        req: request,
-        router: appRouter,
-        createContext: () =>
-          createContext({ req: request, env, workerCtx: ctx }),
-      });
-    }
-    return env.ASSETS.fetch(request);
+    return App.fetch(request, env, ctx);
   },
 } satisfies ExportedHandler<ServiceBindings>;
